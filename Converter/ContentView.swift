ead89260e32e48d7e48049329b6be69e5c579f84
Matt Dias <matt.dias6@gmail.com>
2021-07-15 19:36:57 -0400

    Added functionality to convert user input (Fahrenheit) to celsius

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -7,10 +7,30 @@
 
 import SwiftUI
 
+// goal: Convert Fahrenheit to Celsius
+// (32°F − 32) × 5/9 = 0°C
+// goal achieved!
+
 struct ContentView: View {
+    @State var userInput = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            TextField("Temperature in °F", text: $userInput)
+            Text("\(convertedInput)")
+        }
+        .padding()
+    }
+
+    var convertedInput: String {
+        // could be nil (no value) or a number
+        if let inputAsNumber = Double(userInput) {
+            let result = (inputAsNumber - 32) * 5/9
+
+            return "\(result) °C"
+        } else {
+            return "⚠️ enter a #"
+        }
     }
 }
 
