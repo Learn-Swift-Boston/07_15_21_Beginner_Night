73600354100b40ba6dd3f251297de2728afe9baf
Matt Dias <matt.dias6@gmail.com>
2021-07-15 20:34:40 -0400

    Added ability to convert bi-directionally between Fahrenheit and Celsius
    exercise for the viewer:
       add ability to convert to Kelvin
       add ability to convert to Rankine scale

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -7,16 +7,77 @@
 
 import SwiftUI
 
-// goal: Convert Fahrenheit to Celsius
-// (32°F − 32) × 5/9 = 0°C
-// goal achieved!
+// CaseIterable gives us all the cases of our enum in an array
+// [.fahrenheit, .celsius]
+enum TemperatureUnit: String, Identifiable, CaseIterable {
+    case fahrenheit
+    case celsius
+
+    var id: String {
+        return self.rawValue
+    }
+
+    func toSymbol() -> String {
+        switch self {
+            case .fahrenheit:
+                return "°F"
+            case .celsius:
+                return "°C"
+        }
+    }
+
+    func convert(_ input: Double, to toUnit: TemperatureUnit) -> Double {
+        let kelvins = self.toKelvin(input)
+        let output = toUnit.fromKelvin(kelvins)
+
+        return output
+    }
+
+    func toKelvin(_ number: Double) -> Double {
+        switch self {
+            case .fahrenheit:
+                return (number + 459.67) * 5/9
+            case .celsius:
+                return number + 273.15
+        }
+    }
+
+    func fromKelvin(_ number: Double) -> Double {
+        switch self {
+            case .fahrenheit:
+                return (number * 9/5) - 459.67
+            case .celsius:
+                return number - 273.15
+        }
+    }
+}
 
 struct ContentView: View {
     @State var userInput = ""
+    @State var fromUnit: TemperatureUnit = .fahrenheit
+    @State var toUnit: TemperatureUnit = .celsius
 
     var body: some View {
         VStack(alignment: .leading) {
-            TextField("Temperature in °F", text: $userInput)
+            HStack {
+                Text("From unit: ")
+                Picker("Unit:", selection: $fromUnit) {
+                    ForEach(TemperatureUnit.allCases) { unit in
+                        Text(unit.rawValue.capitalized).tag(unit)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            HStack {
+                Text("To unit: ")
+                Picker("Unit:", selection: $toUnit) {
+                    ForEach(TemperatureUnit.allCases) { unit in
+                        Text(unit.rawValue.capitalized).tag(unit)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            TextField("Temperature in \(fromUnit.toSymbol())", text: $userInput)
             Text("\(convertedInput)")
         }
         .padding()
@@ -24,10 +85,11 @@ struct ContentView: View {
 
     var convertedInput: String {
         // could be nil (no value) or a number
-        if let inputAsNumber = Double(userInput) {
-            let result = (inputAsNumber - 32) * 5/9
+        if let inputAsNumber = Double(userInput),
+           case let result = fromUnit.convert(inputAsNumber, to: toUnit),
+           let formattedNumber = NumberFormatter().string(from: result as NSNumber) {
 
-            return "\(result) °C"
+            return "\(formattedNumber) \(toUnit.toSymbol())"
         } else {
             return "⚠️ enter a #"
         }
